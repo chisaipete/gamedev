85e20471f3b3240b83252fba83e25fc5bf9913d0
Connor Petersen <cmpete@gmail.com>
2019-01-14 07:42:05 -0800

    working through lesson 1

--- tiny.cpp
+++ tiny.cpp
@@ -51,9 +51,49 @@ bool close() {
     return true;
 }
 
+void line(int x0, int y0, int x1, int y1, RawTexture &i, SDL_Color c) {
+    // FIRST ATTEMPT
+    // for (float t = 0; t < 1; t += .01) {
+    //     int x = x0*(1-t) + x1*t;
+    //     int y = y0*(1-t) + y1*t;
+    //     i.set(x, y, c);
+    // }
+
+    // SECOND ATTEMPT
+    // for (int x = x0; x <= x1; x++) {
+    //     float t = (x-x0) / (float)(x1-x0);
+    //     int y = y0*(1-t) + y1*t;
+    //     i.set(x, y, c);
+    // }
+
+    // THIRD ATTEMPT
+    bool steep = false;
+    if (std::abs(x0-x1) < std::abs(y0-y1)) { //if the line is steep, transpose
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
+    }
+    if (x0>x1) { //make it left to right
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+    for (int x = x0; x <= x1; x++) {
+        float t = (x-x0) / (float)(x1-x0);
+        int y = y0*(1-t) + y1*t;
+        if (steep) {
+            i.set(y, x, c); //if transposed, de-transpose
+        } else {
+            i.set(x, y, c);
+        }
+    }
+}
+
 void render() {
     image.lock_texture();
-    image.set(52, 41, RED);
+    // image.set(52, 41, RED);
+    line(13, 20, 80, 40, image, WHITE);
+    line(20, 13, 40, 80, image, RED);
+    line(80, 40, 13, 20, image, RED);
     image.unlock_texture();
 }
 
