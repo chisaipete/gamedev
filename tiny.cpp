e2540b46952377ecd7095820503ba30f8ad10adc
Connor Petersen <cmpete@gmail.com>
2019-01-15 17:03:28 -0800

    shifting back to given algorithm

--- tiny.cpp
+++ tiny.cpp
@@ -72,43 +72,34 @@ bool close() {
     return true;
 }
 
-void rc_line(int x0, int y0, int x1, int y1, RawTexture &i, SDL_Color c) {
-    // TODO: Why does this work?
-    // bool steep = false;
-    // if (std::abs(x0-x1) < std::abs(y0-y1)) { //if the line is steep, transpose
-    //     std::swap(x0, y0);
-    //     std::swap(x1, y1);
-    //     steep = true;
-    // }
-    // if (x0>x1) { //make it left to right
-    //     std::swap(x0, x1);
-    //     std::swap(y0, y1);
-    // }
-    // int dx = x1-x0;
-    // int dy = y1-y0;
-    // int derror2 = std::abs(dy)*2;
-    // int error2 = 0;
-    // int y = y0;
-    // for (int x = x0; x <= x1; x++) {
-    //     // if (steep) {
-    //     //     i.set(y, x, c); //if transposed, de-transpose
-    //     // } else {
-    //         i.set(x, y, c);
-    //     // }
-    //     error2 += derror2;
-    //     if (error2 > dx) {
-    //         y += (y1 > y0 ? 1 : -1);
-    //         error2 -= dx*2;
-    //     }
-    // }
-}
-
 void line(int x0, int y0, int x1, int y1, RawTexture &i, SDL_Color c) {
-    int slope = (y1-y0)/(x1-x0);
-    int y = 0;
-    for (int x = xl; x <= x1; x++) {
-        int y = std::round(slope*x + offset);
-        i.set(x, y, c);
+    // TODO: Why does this work?
+    bool steep = false;
+    if (std::abs(x0-x1) < std::abs(y0-y1)) { //if the line is steep, transpose
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
+    }
+    if (x0>x1) { //make it left to right
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+    int dx = x1-x0;
+    int dy = y1-y0;
+    int derror2 = std::abs(dy)*2;
+    int error2 = 0;
+    int y = y0;
+    for (int x = x0; x <= x1; x++) {
+        if (steep) {
+            i.set(y, x, c); //if transposed, de-transpose
+        } else {
+            i.set(x, y, c);
+        }
+        error2 += derror2;
+        if (error2 > dx) {
+            y += (y1 > y0 ? 1 : -1);
+            error2 -= dx*2;
+        }
     }
 }
 
